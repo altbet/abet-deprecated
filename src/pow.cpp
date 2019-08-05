a6a6ff5f8107f0741cfdf5fb309b78bbc012afee
Travis <travisfinch01@gmail.com>
2019-08-05 12:00:29 -0400

    test

--- src/pow.cpp
+++ src/pow.cpp
@@ -38,8 +38,8 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 
     if (pindexLast->nHeight > Params().LAST_POW_BLOCK()) {
         uint256 bnTargetLimit = (~uint256(0) >> 24);
-        int64_t nTargetSpacing = 60;
-        int64_t nTargetTimespan = 60 * 40;
+        int64_t nTargetSpacing = 120;
+        int64_t nTargetTimespan = 120 * 40;
 
 		if (pindexLast->nHeight <= 10580 && pindexLast->nHeight > 4000) {
 			nTargetSpacing = 180;
